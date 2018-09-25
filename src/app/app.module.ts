90562cf1af0e8591e9a781268469b051167c9c1d
Enrique Sapien <enrique.sapien@accenture.com>
2018-09-25 18:25:36 -0500

    Cleanup for new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { UsernameComponent } from './username/username.component';
-import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    UsernameComponent,
-    DirectivesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
