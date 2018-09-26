8f8074c2ff697059c256c861a27669defaeaa51f
Enrique Sapien <esapien@hotmail.com>
2018-09-25 22:48:38 -0500

    Restore components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,6 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { UsernameComponent } from './username/username.component';
 import { DirectivesComponent } from './directives/directives.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -25,14 +18,7 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
     WarningAlertComponent,
     SuccessAlertComponent,
     UsernameComponent,
-    DirectivesComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    RecipeBookComponent,
-    ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
